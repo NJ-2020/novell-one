ed2771a1851377253a5a6307245ec0aa1503622f
NJ-2020 <76243292+NJ-2020@users.noreply.github.com>
2021-08-24 13:35:38 +0700

    Give reference html section to javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.menu_toggle');
+const navul = document.querySelector('nav ul');
+const body = document.querySelector('body');
+const menuLink = document.querySelector('nav ul li a');
+const main = document.querySelector('.main');
+const navbar = document.querySelector('nav');
+const imgClose = document.querySelector('.img-close');
+const imgOpen = document.querySelector('.img-open');
