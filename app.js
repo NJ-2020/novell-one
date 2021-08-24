89d1f554b3bef749e198a7675bd2da32703a9a5c
NJ-2020 <76243292+NJ-2020@users.noreply.github.com>
2021-08-24 13:40:21 +0700

    Add javascript code for some section in html

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const main = document.querySelector('.main');
 const navbar = document.querySelector('nav');
 const imgClose = document.querySelector('.img-close');
 const imgOpen = document.querySelector('.img-open');
+
+menuToggle.addEventListener("click", () => {
+  navul.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+  body.classList.toggle('stop-scrolling');
+  main.classList.toggle('blur');
+  navbar.classList.remove('scroll-active');
+  imgClose.classList.toggle('active');
+  imgOpen.classList.toggle('active');
+})
